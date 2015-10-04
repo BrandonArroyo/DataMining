dd5a71c82098041413529019c99385483f41a7ee
BrandonArroyo <barroyo50@gmail.com>
2015-10-04 14:23:26 -0500

    scraps title and links

--- myproject/myproject/spiders/mydomain.py
+++ myproject/myproject/spiders/mydomain.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from myproject.items import MyprojectItem
 
 class MydomainSpider(scrapy.Spider):
     name = "mydomain"
-    allowed_domains = ["mydomain.com"]
-    start_urls = (
-        'http://www.mydomain.com/',
-    )
+    allowed_domains = ["snopes.com"]
+    start_urls = [
+        "http://www.snopes.com/info/articles.asp"
+    ]
 
     def parse(self, response):
-        pass
+        for sel in response.xpath('//ul/li'):#selection of the catagories
+            item = MyprojectItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            yield item
